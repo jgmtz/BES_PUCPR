204d3a192e22f6807a38cb8b02f0bf59ebb80105
jgmtz <joaoguimatz@gmail.com>
2024-04-25 10:07:43 -0300

    game

--- Projects/python/pygame/@game.py
+++ Projects/python/pygame/@game.py
@@ -1,42 +1,67 @@
-import sys
-
 import pygame
+from pygame.locals import *
+from sys import exit
+import pygame.locals 
+from random import randint
 
-class game:
-    def __init__(self):      
-        pygame.init()
-        screen_width = 1280
-        screen_height = 640
+largura_obj = 100
+altura_obj = 100
+largura = 1280
+altura = 960
+x = largura/2 - largura_obj/2
+y = altura/2 - altura_obj/2
+x_azul = randint(20, 1200)
+y_azul = randint(30,860)
+pygame.font.init()
+pontos = 0
+fonte = pygame.font.SysFont('Arial', 60, True, True)
+
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption('jogo')
+clock = pygame.time.Clock()
+
+while True: 
+    clock.tick(30)
+    tela.fill((255,0,0))
+    mensagem = f'Pontos: {pontos}'
+    texto_formatado = fonte.render(mensagem, False, (255,255,255))
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit(
+                exit()
+            )
+        '''
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+               x = x - 40
+            if event.key == K_d:
+                x = x + 40
+            if event.key == K_w:
+                y = y - 40
+            if event.key == K_s:
+                y = y + 40'''
+    if pygame.key.get_pressed()[K_a]:
+        x = x - 20   
+    if pygame.key.get_pressed()[K_d]:
+        x = x + 20
+    if pygame.key.get_pressed()[K_w]:
+        y = y - 20
+    if pygame.key.get_pressed()[K_s]:
+        y = y + 20      
+    ret_vermelho = pygame.draw.rect(tela, (255,0,0),(x, y, largura_obj, altura_obj))
+    ret_azul = pygame.draw.rect(tela, (0,0,255), (x_azul, y_azul, largura_obj,))
+    if ret_vermelho.colliderect(ret_azul):
+        x_azul = randint(40,600)
+        y_azul =randint(50,430)
+        pontos = pontos + 1
         
-        pygame.display.set_caption('jogo')
-        self.screen = pygame.display.set_mode((screen_width, screen_height))
-        
-        self.clock = pygame.time.Clock()
-        
-        self.img = pygame.image.load('cloud1.jpg')
-        
-        self.img_pos = [160,260]
-        self.movement = [False,False]
-    def run(self):
-        while True:
-            self.img_pos[1] += (self.movement[1] - self.movement[0])*5
-            self.screen.blit(self.img, self.img_pos)
-            
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w:
-                        self.movement[0] = True
-                    if event.key == pygame.K_s:
-                        self.movement[1] = True
-                    if event.key == pygame.K_w:
-                        self.movement[0] = False
-                    if event.key == pygame.K_s:
-                        self.movement[1] = False
-            
-            pygame.display.update()
-            self.clock.tick(60)          
-game().run()
-            
+    tela.blit(texto_formatado, (900, 80))
+    pygame.display.update()   
+    if y >= altura:
+        y = 0
+    if y < 0:
+        y = altura
+    if x >= largura:
+        x = 0
+    if x < 0:
+        x = largura
